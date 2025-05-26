96af73d10c4d91d4e295d8a8ab09cb62b7bf3627
Illia16 <illia2143@gmail.com>
2025-05-26 17:25:10 -0400

    google auth: oauth2 wip

--- auth/google_auth_oauth2.js
+++ auth/google_auth_oauth2.js
@@ -1,4 +1,15 @@
+const { google } = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI,
+);
+
 exports.handler = async (event) => {
+    console.log('process.env.GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID);
+    console.log('process.env.GOOGLE_CLIENT_SECRET', process.env.GOOGLE_CLIENT_SECRET);
+    console.log('process.env.GOOGLE_REDIRECT_URI', process.env.GOOGLE_REDIRECT_URI);
 
     // Common CORS headers
     const corsHeaders = {
@@ -11,17 +22,49 @@ exports.handler = async (event) => {
     try {
         // Log the incoming event
         console.log('Event:', JSON.stringify(event, null, 2));
+        const code = event?.queryStringParameters?.code;
+        console.log('code');
+
+        if (!code) {
+            return {
+                statusCode: 400,
+                headers: corsHeaders,
+                body: JSON.stringify({ message: 'Missing code parameter' }),
+            };
+        }
+
+        // const { tokens } = await oauth2Client.getToken(code)
+        const { tokens } = await oauth2Client.getToken({code, redirect_uri: process.env.GOOGLE_REDIRECT_URI})
+        console.log('tokens', tokens);
+        oauth2Client.setCredentials(tokens);
+
+        const oauth2 = google.oauth2({
+            auth: oauth2Client,
+            version: 'v2',
+        });
+        const { data: user } = await oauth2.userinfo.get();
+
+        console.log('user', user);
+
+        const redirectUrl = `http://localhost:3000?name=${encodeURIComponent(
+        user.name
+        )}&email=${encodeURIComponent(user.email)}&picture=${encodeURIComponent(user.picture)}`;
 
         return {
-            statusCode: 200,
-            headers: corsHeaders,
-            body: JSON.stringify({
-                message: 'Authentication successful',
-                event: event
-            })
+            statusCode: 302,
+            headers: {
+                Location: redirectUrl,
+                ...corsHeaders,
+            },
+            body: JSON.stringify({ message: "Redirecting..." }), 
         };
+        // return {
+        //     statusCode: 200,
+        //     headers: corsHeaders,
+        //     body: JSON.stringify({code: code}),
+        // };
     } catch (error) {
-        console.error('Error:', error);
+        console.error('OAuth error:', error);
         return {
             statusCode: 500,
             headers: corsHeaders,
